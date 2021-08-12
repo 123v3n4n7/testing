76569e8966baca7402f428c815aab354c34a5416
123v3n4n7 <eugenezharikov@gmail.com>
2021-08-12 20:51:19 +0300

    Обновил settings.py

--- testing/settings.py
+++ testing/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'testing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'stage',
-        'USER': 'postgres',
-        'PASSWORD': 'secret',
-        'HOST': 'pg_database',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+# DATABASES = {
+#
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'stage',
+#         'USER': 'postgres',
+#         'PASSWORD': 'secret',
+#         'HOST': 'pg_database',
+#         'PORT': 5432,
+#     }
+#
+# }
 
 
 # Password validation
