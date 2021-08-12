1e91ef97dd5eb9e5362633a2b6ce7ae0627d9e96
123v3n4n7 <eugenezharikov@gmail.com>
2021-08-12 21:27:22 +0300

    add whitenoise

--- testing/settings.py
+++ testing/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'whitenoise',
     'knox',
     'test_app'
 ]
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'testing.urls'
