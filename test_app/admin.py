8c9ae8be33b0800e597ad20e8f5c4b995b2a65fb
123v3n4n7 <eugenezharikov@gmail.com>
2021-08-13 14:21:15 +0300

    Отредактировал админку

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,30 +1,40 @@
 from django.contrib import admin
 from .models import Question, Choice, ListOfQuestion, Answer
 
-admin.site.register(Choice)
-#admin.site.register(Answer)
-
 
 class QuestionsInLine(admin.TabularInline):
     model = Question
+    extra = 0
 
 
 class ChoicesInline(admin.TabularInline):
     model = Choice
+    extra = 0
+
+
+@admin.register(Choice)
+class Choice(admin.ModelAdmin):
+    list_filter = ['question']
+    search_fields = ['question__title']
+    list_display = ('title', 'question', 'is_right', 'id',)
 
 
 @admin.register(ListOfQuestion)
 class ListOfQuestion(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'id',)
     inlines = [QuestionsInLine]
 
 
 @admin.register(Question)
 class Question(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'list_of_question', 'id',)
+    list_filter = ['list_of_question']
+    search_fields = ['list_of_question__title']
     inlines = [ChoicesInline]
 
 
 @admin.register(Answer)
 class Answer(admin.ModelAdmin):
-    list_display = ('id', 'user', 'test_id', 'percent_of_corr_answers', 'num_of_corr_answers', 'num_of_incorr_answers')
+    list_filter = ['user']
+    search_fields = ['user']
+    list_display = ('user', 'test_id', 'percent_of_corr_answers', 'num_of_corr_answers', 'num_of_incorr_answers', 'id',)
